3778fcc0bbb6668aa547fb3778699ead9be47b39
Ichsan Private <Ichsan Private@DESKTOP-9EB55H6>
2017-04-28 11:02:05 +0700

    insert web security and fix index user

---
+++ src/main/java/com/loundry/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+//package com.loundry.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//
+//@Configuration
+//public class WebMvcConfig extends WebMvcConfigurerAdapter {
+//	
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry routes) {
+//		routes.addViewController("/403").setViewName("403");
+//		routes.addViewController("/login").setViewName("login");
+//	}
+//
+//}
