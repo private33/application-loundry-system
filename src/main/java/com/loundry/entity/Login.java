c2ec587811451b717d625f32049a0b2ddb150fc5
Risky Miftahul Fajri <Risky Miftahul Fajri@DESKTOP-MNKR7V2>
2017-05-02 17:08:35 +0700

    entity login

--- src/main/java/com/loundry/entity/Login.java
+++ src/main/java/com/loundry/entity/Login.java
@@ -27,5 +27,39 @@ public class Login {
 	@ManyToOne
 	@JoinColumn(name="role_id", nullable=false)
 	private Roles role;
+
+	public int getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(int loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Roles getRole() {
+		return role;
+	}
+
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+	
+	
 	
 }
