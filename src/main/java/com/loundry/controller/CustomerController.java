3613f5e3bd4bd0fa1ddf57c8ff17cbe49bbee8b7
Risky Miftahul Fajri <Risky Miftahul Fajri@DESKTOP-MNKR7V2>
2017-05-02 16:49:07 +0700

    Customer

--- src/main/java/com/loundry/controller/CustomerController.java
+++ src/main/java/com/loundry/controller/CustomerController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ConsumenController {
+public class CustomerController {
 
 	@RequestMapping("/")
 	public String goHome() {
